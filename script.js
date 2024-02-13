87e5770887bdef0f3017963da9ff3f218b93bf07
Shivam2p <122889419+Shivam2p@users.noreply.github.com>
2024-02-13 12:36:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+let secretNumber=Math.trunc(Math.random()*20)+1;
+let score=20;
+let highscore=0;
+let msg=document.querySelector('.message');
+let agn= document.querySelector(".again");
+
+const displayMessage=function(message)
+{
+    msg.textContent=message;
+}
+document.querySelector('.check').addEventListener('click',function(){
+    const guess =Number(document.querySelector('.guess').value);
+    console.log(guess,typeof guess);
+
+    if(!guess)
+    {
+        displayMessage("⛔no number");
+    }
+    else if(guess===secretNumber)
+    {
+        displayMessage("you won 🏆")
+        document.querySelector(".number").textContent=secretNumber;
+        document.querySelector('body').style.backgroundColor='#ec4e20'
+    }
+    else if (guess !== secretNumber) {
+        if (score > 1) {
+          // document.querySelector('.message').textContent =
+          // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+          displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+          score--;
+          document.querySelector('.score').textContent = score;
+        } else {
+          // document.querySelector('.message').textContent = '💥 You lost the game!';
+          displayMessage('💥 You lost the game!');
+          document.querySelector('.score').textContent = 0;
+        }
+      }
+})
+agn.addEventListener('click', function(){
+  score=20;
+  secretNumber=Math.trunc(Math.random()*20)+1;
+  displayMessage("start guessing");
+  document.querySelector("body").style.backgroundColor='#273239'
+
+})
